cdc1b1e27c528e74a55dc7e8a451d381e02719e0
“Young <hogusong1@gmail.com>
2019-02-27 02:10:05 -0500

    added 'image loader'

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -38,6 +38,17 @@ export class PostCreateComponent implements OnInit {
     } 
   }
 
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    // this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+ 
   onSavePost() {
     if (this.form.invalid) {  return  }
     const newTitle = this.form.value.title.trim();
