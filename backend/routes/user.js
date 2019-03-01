a5055240cbaeb651b4aa7cfffedb70627eaaa728
“Young <hogusong1@gmail.com>
2019-03-01 14:18:06 -0500

    added 'add user' router in serve side connecting mongoose and add 'bcrypt'.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const bcrypt =  require('bcrypt');
+
 const router = express.Router();
 
-router.post('/api/signup', (req, res, next) => {
+const USER = require('../models/user');
 
+router.post('/api/signup', (req, res, next) => {
+  const password = bcrypt.hash(req.body.password, 10).then(hash => hash);
+  if (password) {
+    const user = new USER({
+      email: req.body.email,  password: password
+    });
+    user.save().then(result => {
+      res.status(201).json({
+        message: 'Signup successfully.',
+        id: result._id
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+  }
 })
 
 
