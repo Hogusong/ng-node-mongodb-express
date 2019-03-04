3f11aa7496847566ea884baa888b0938cfb96311
“Young <hogusong1@gmail.com>
2019-03-03 21:19:18 -0500

    created 'NgMaterialModule' to separate all MaterialModule from 'AppModule'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule,
-         MatCardModule,
-         MatButtonModule,
-         MatToolbarModule,
-         MatExpansionModule,
-         MatProgressSpinnerModule, 
-         MatPaginatorModule,
-         MatDialogModule} from '@angular/material';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -22,6 +15,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { NgMaterialModule } from './ng-material.module';
 
 @NgModule({
   declarations: [
@@ -39,14 +33,7 @@ import { ErrorComponent } from './error/error.component';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
+    NgMaterialModule,
     AppRoutingModule
   ],
   providers: [
